6c59fa30b236a8af2f42785bc9f6e50d7fea1b25
himashrimehra <himashrimehra@gmail.com>
2023-09-08 19:29:35 +0530

    changed forgot password page

--- frontend/my-app/src/Login.jsx
+++ frontend/my-app/src/Login.jsx
@@ -14,7 +14,7 @@ export const Login = (props) => {
             <h2>Login</h2>
             <form className="login-form" onSubmit={handleSubmit}>
                 <label htmlFor="email">Email</label>
-                <input value={email} onChange={(e) => setEmail(e.target.value)}type="email" placeholder="youremail@gmail.com" id="email" name="email" />
+                <input value={email} onChange={(e) => setEmail(e.target.value)}type="email" placeholder="youremail@email.com" id="email" name="email" />
                 <label htmlFor="password">Password</label>
                 <input value={pass} onChange={(e) => setPass(e.target.value)} type="password" placeholder="********" id="password" name="password" />
                 <button type="submit">Log In</button>
--- frontend/my-app/src/Register.jsx
+++ frontend/my-app/src/Register.jsx
@@ -17,10 +17,10 @@ export const Register = (props) => {
             {/* <label htmlFor="name">Full Name</label>
             <input value={name} name="name" onChange={(e) => setName(e.target.value)} id="name" placeholder="full Name" /> */}
             <label htmlFor="email">Email</label>
-            <input value={email} onChange={(e) => setEmail(e.target.value)}type="email" placeholder="youremail@gmail.com" id="email" name="email" />
-            <label htmlFor="password">Enter new password</label>
-            <input value={pass} onChange={(e) => setPass(e.target.value)} type="password" placeholder="********" id="password" name="password" />
-            <button type="submit">Log In</button>
+            <input value={email} onChange={(e) => setEmail(e.target.value)}type="email" placeholder="youremail@email.com" id="email" name="email" />
+            {/* <label htmlFor="password">Enter new password</label>
+            <input value={pass} onChange={(e) => setPass(e.target.value)} type="password" placeholder="********" id="password" name="password" /> */}
+            <button type="submit">Send OTP for verification</button>
         </form>
         <button className="link-btn" onClick={() => props.onFormSwitch('login')}>Already have an account? Login here.</button>
     </div>
