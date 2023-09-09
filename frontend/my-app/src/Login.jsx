829ee0a9f2730aa3190f1164436ad14b81aedb8d
himashrimehra <himashrimehra@gmail.com>
2023-09-09 20:33:24 +0530

    added validation to login

--- frontend/my-app/src/Login.jsx
+++ frontend/my-app/src/Login.jsx
@@ -1,22 +1,63 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 
 export const Login = (props) => {
-    const [email, setEmail] = useState('');
-    const [pass, setPass] = useState('');
+    const initialValues = { email: "", password: "" };
+    const [formValues, setFormValues] = useState(initialValues);
+    const [formErrors, setFormErrors] = useState({});
+    const [isSubmit, setIsSubmit] = useState(false); //flag for submit
 
     const handleSubmit = (e) => {
         e.preventDefault();
-        console.log(email);
+        // console.log(email);
+        setFormErrors(validate(formValues));
+        setIsSubmit(true);
     }
+
+    const handleChange = (e) => {
+        const { name, value } = e.target;
+        setFormValues({ ...formValues, [name]: value });
+    };
+
+    useEffect(() => {
+        console.log(formErrors);
+        if (Object.keys(formErrors).length === 0 && isSubmit) {
+          console.log(formValues);
+        }
+      }, [formErrors]); //will get changed based on formErrors
+
+    const validate = (values) => {
+        const errors = {};
+        const regex = /^[^\s@]+@[^\s@]+\.[^\s@]{2,}$/i;
+        if (!values.email) {
+          errors.email = "Email is required!";
+        } else if (!regex.test(values.email)) {
+          errors.email = "This is not a valid email format!";
+        }
+        if (!values.password) {
+          errors.password = "Password is required!";
+        } else if (values.password.length < 8) {
+          errors.password = "Password must be more than 8 characters.";
+        } else if (values.password.length > 16) {
+          errors.password = "Password cannot exceed more than 16 characters.";
+        }
+        return errors;
+      };    
 
     return (
         <div className="auth-form-container">
+            {Object.keys(formErrors).length === 0 && isSubmit ? (
+                <div className="ui message success">Signed in successfully</div>
+            ) : (
+                console.log("")
+            )}
             <h2>Login</h2>
             <form className="login-form" onSubmit={handleSubmit}>
                 <label htmlFor="email">Email</label>
-                <input value={email} onChange={(e) => setEmail(e.target.value)}type="email" placeholder="youremail@email.com" id="email" name="email" />
+                <input value={formValues.email} onChange={handleChange} type="email" placeholder="youremail@email.com" id="email" name="email" />
+                <p>{formErrors.email}</p>
                 <label htmlFor="password">Password</label>
-                <input value={pass} onChange={(e) => setPass(e.target.value)} type="password" placeholder="********" id="password" name="password" />
+                <input value={formValues.password} onChange={handleChange} type="password" placeholder="********" id="password" name="password" />
+                <p>{formErrors.password}</p>
                 <button type="submit">Log In</button>
             </form>
             <button className="link-btn" onClick={() => props.onFormSwitch('register')}>Forgot Password?</button>
--- frontend/my-app/src/Register.jsx
+++ frontend/my-app/src/Register.jsx
@@ -1,23 +1,58 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 
 export const Register = (props) => {
-    const [email, setEmail] = useState('');
-    const [pass, setPass] = useState('');
-    const [name, setName] = useState('');
+    // const [email, setEmail] = useState('');
+    // const [pass, setPass] = useState('');
+    // const [name, setName] = useState('');
+    const initialValues = { email: "" };
+    const [formValues, setFormValues] = useState(initialValues);
+    const [formErrors, setFormErrors] = useState({});
+    const [isSubmit, setIsSubmit] = useState(false);
 
     const handleSubmit = (e) => {
         e.preventDefault();
-        console.log(email);
+        // console.log(email);
+        setFormErrors(validate(formValues));
+        setIsSubmit(true);
     }
+
+    const handleChange = (e) => {
+        const { name, value } = e.target;
+        setFormValues({ ...formValues, [name]: value });
+    };
+
+    useEffect(() => {
+        console.log(formErrors);
+        if (Object.keys(formErrors).length === 0 && isSubmit) {
+          console.log(formValues);
+        }
+      }, [formErrors]); 
+
+      const validate = (values) => {
+        const errors = {};
+        const regex = /^[^\s@]+@[^\s@]+\.[^\s@]{2,}$/i;
+        if (!values.email) {
+          errors.email = "Email is required!";
+        } else if (!regex.test(values.email)) {
+          errors.email = "This is not a valid email format!";
+        }
+        return errors;
+      };    
 
     return (
         <div className="auth-form-container">
+        {Object.keys(formErrors).length === 0 && isSubmit ? (
+                <div className="ui message success">OTP sent to the requested Email ID.</div>
+            ) : (
+                console.log("")
+            )}
             <h2>Reset Password</h2>
         <form className="register-form" onSubmit={handleSubmit}>
             {/* <label htmlFor="name">Full Name</label>
             <input value={name} name="name" onChange={(e) => setName(e.target.value)} id="name" placeholder="full Name" /> */}
             <label htmlFor="email">Email</label>
-            <input value={email} onChange={(e) => setEmail(e.target.value)}type="email" placeholder="youremail@email.com" id="email" name="email" />
+            <input value={formValues.email} onChange={handleChange}type="email" placeholder="youremail@email.com" id="email" name="email" />
+            <p>{formErrors.email}</p>
             {/* <label htmlFor="password">Enter new password</label>
             <input value={pass} onChange={(e) => setPass(e.target.value)} type="password" placeholder="********" id="password" name="password" /> */}
             <button type="submit">Send OTP for verification</button>
