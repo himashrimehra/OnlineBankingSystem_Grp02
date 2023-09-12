23077e9c1290a24140dd135631c71ec5f9cc51ec
garima2610 <78798393+garima2610@users.noreply.github.com>
2023-09-12 20:14:59 +0530

    Merging login into main (#3)
    * adding login page
    
    * changed forgot password page
    
    * added validation to login
    
    * added register page
    
    * Added Backend functionality
    
    * Backend updated
    
    * Backend works
    
    * New account page added
    
    * untangle the mess
    
    ---------
    
    Co-authored-by: himashrimehra <himashrimehra@gmail.com>
    Co-authored-by: Saransh Rajput <saransh.rajput@research.iiit.ac.in>
    Co-authored-by: himashrimehra <76397754+himashrimehra@users.noreply.github.com>

--- frontend/my-app/src/App.js
+++ frontend/my-app/src/App.js
@@ -1,23 +1,32 @@
+import React, { useState } from "react";
 import logo from './logo.svg';
 import './App.css';
+import { Login } from "./Login";
+import { ForgotPassword } from "./ForgotPassword";
+import {Register} from "./Register";
+import { BrowserRouter,Routes,Route } from "react-router-dom";
+import {NewAccount} from "./NewAccount";
 
 function App() {
+  const [currentForm, setCurrentForm] = useState('login');
+
+  const toggleForm = (formName) => {
+    setCurrentForm(formName);
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {/* {
+        currentForm === "login" ? <Login onFormSwitch={toggleForm} /> : <ForgotPassword onFormSwitch={toggleForm} />
+      } */}
+      <BrowserRouter>
+            <Routes>
+              <Route path="/" element= { <Login/>} />
+              <Route path="/forgotpassword" element= { <ForgotPassword/>} />
+              <Route path="/register" element= { <Register/>} />
+              <Route path="/newaccount" element= { <NewAccount/>} />
+            </Routes>
+        </BrowserRouter>
     </div>
   );
 }
